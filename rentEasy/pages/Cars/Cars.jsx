1c6edcae479735804c266d61f3ba5499acb8c7a7
ABISHEK <abideepu19.vrs@gmail.com>
2025-03-25 19:15:55 +0530

    Working Filter Navbar in Cars List Page

--- rentEasy/pages/Cars/Cars.jsx
+++ rentEasy/pages/Cars/Cars.jsx
@@ -1,15 +1,33 @@
 import React from "react";
 import data from "../data.js";
-import { Link } from "react-router";
+import { Link, useSearchParams } from "react-router";
 export default function Cars() {
   const [cars, setCars] = React.useState([]);
+  const [searchParams, setSearchParams] = useSearchParams();
+
+  const typeFilter = searchParams.get("type");
+
+  const displayedCars = typeFilter
+    ? cars.filter((car) => car.type == typeFilter)
+    : cars;
 
   React.useEffect(() => {
     setCars(data);
     console.log(cars);
   }, []);
 
-  const carsEl = cars.map((car) => {
+  function handleFilterChange(key, value) {
+    setSearchParams((prevParams) => {
+      if (value === null) {
+        prevParams.delete(key);
+      } else {
+        prevParams.set(key, value);
+      }
+      return prevParams;
+    });
+  }
+
+  const carsEl = displayedCars.map((car) => {
     let typeClass;
 
     if (car.type == "luxury") {
@@ -41,9 +59,41 @@ export default function Cars() {
 
   return (
     <div className="cars-container">
-      {/* <div className="cars-header">
-
-      </div> */}
+      <h1>Explore our Car options</h1>
+      <div className="car-list-filter-buttons">
+        <button
+          onClick={() => handleFilterChange("type", "simple")}
+          className={`car-list-type simple ${
+            typeFilter == "simple" ? "selected" : null
+          }`}
+        >
+          Simple
+        </button>
+        <button
+          onClick={() => handleFilterChange("type", "luxury")}
+          className={`car-list-type luxury ${
+            typeFilter == "luxury" ? "selected" : null
+          }`}
+        >
+          Luxury
+        </button>
+        <button
+          onClick={() => handleFilterChange("type", "economy")}
+          className={`car-list-type economy ${
+            typeFilter == "economy" ? "selected" : null
+          }`}
+        >
+          Economy
+        </button>
+        {typeFilter != null && (
+          <button
+            onClick={() => handleFilterChange("type", null)}
+            className="clear-filters"
+          >
+            Clear filter
+          </button>
+        )}
+      </div>
       <div className="cars-list-div">{carsEl}</div>
     </div>
   );
